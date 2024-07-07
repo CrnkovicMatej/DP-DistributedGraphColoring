3173c24ec00273631671c14803825832e50a5b24
aiivka <ivkacaci.math@pmf.hr>
2024-07-07 20:29:31 +0200

    add lubyMIS algoritm, add graph examples

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,8 +1,10 @@
 package com.project;
 
-import com.project.network.Network;
-import com.project.network.Node;
-import com.project.utils.NetworkLoader;
+import com.project.mis_algorithm.*;
+
+import com.project.network.*;
+import com.project.utils.*;
+import java.util.List;
 
 import java.io.IOException;
 
@@ -18,29 +20,49 @@ public class Main {
         String algorithm = args[2];
         int maxColors = args.length >= 4 ? Integer.parseInt(args[3]) : Integer.MAX_VALUE;
 
-        NetworkLoader loader = new NetworkLoader(nodesFilePath, linksFilePath);
-        Network network;
+        if ("maximal".equals(algorithm)) { 
+            try {
+                List<LubyMIS.Node> nodes = LubyMIS.readNodes(nodesFilePath);
+                LubyMIS.readLinks(linksFilePath, nodes);
 
-        try {
-            network = loader.loadTopology();
-        } catch (IOException e) {
-            System.out.println("Error loading network topology: " + e.getMessage());
-            return;
+                // Execute Luby's algorithm
+                LubyMIS.lubyMIS(nodes, 4);
+
+                // Print the result
+                System.out.println("Maximal Independent Set (MIS) results:");
+                for (LubyMIS.Node node : nodes) {
+                    if (node.getInMIS().get()) {
+                        System.out.println("Node " + node.getId() + " is in the MIS.");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            NetworkLoader loader = new NetworkLoader(nodesFilePath, linksFilePath);
+            Network network;
+
+            try {
+                network = loader.loadTopology();
+            } catch (IOException e) {
+                System.out.println("Error loading network topology: " + e.getMessage());
+                return;
+            }
+
+            if (!loader.validateTopology(network)) {
+                System.out.println("The network is not fully connected.");
+                return;
+            }
+
+            for (Node node : network.getNodes()) {
+                // Reinitialize nodes with the algorithm and maxColors
+                Node newNode = new Node(node.getId(), algorithm, maxColors);
+                newNode.getNeighbors().addAll(node.getNeighbors());
+                network.getNodes().set(network.getNodes().indexOf(node), newNode);
+            }
+
+            network.start();
+            System.out.println("Network has started.");
         }
-
-        if (!loader.validateTopology(network)) {
-            System.out.println("The network is not fully connected.");
-            return;
-        }
-
-        for (Node node : network.getNodes()) {
-            // Reinitialize nodes with the algorithm and maxColors
-            Node newNode = new Node(node.getId(), algorithm, maxColors);
-            newNode.getNeighbors().addAll(node.getNeighbors());
-            network.getNodes().set(network.getNodes().indexOf(node), newNode);
-        }
-
-        network.start();
-        System.out.println("Network has started.");
     }
 }
