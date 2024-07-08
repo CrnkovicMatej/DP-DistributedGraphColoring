0a29eec7bb9b905a6b1654f38ceb99b3057e9b48
ivkacaci.math@pmf.hr <Robek411>
2024-07-08 23:36:46 +0200

    add luby alg to node class

--- src/com/project/network/Message.java
+++ src/com/project/network/Message.java
@@ -1,11 +1,26 @@
 package com.project.network;
 
 public class Message {
+
+    public enum Type { 
+        DEFAULT,
+        RANDOM,
+        SELECTED
+    }
+
+    public Type type = Type.DEFAULT;
     private String content;
     private Node source;
     private Node destination;
 
     public Message(String content, Node source, Node destination) {
+        this.content = content;
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public Message(Type type, String content, Node source, Node destination) {
+        this.type = type;
         this.content = content;
         this.source = source;
         this.destination = destination;
